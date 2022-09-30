9c3886c39de2fe38f2d7982f7b75cb96ba89a16f
ftm1337 <84405345+ftm1337@users.noreply.github.com>
2022-09-30 07:27:09 -0400

    Update [id].ts

--- api/users/[id].ts
+++ api/users/[id].ts
@@ -1,8 +1,13 @@
 import { NowRequest, NowResponse } from "@vercel/node";
 
 const users = ["Bob Smith", "Guillame Bibeau"];
+async function f1() {
+    const response = await fetch('https://api.llama.fi/protocol/guru-network');
+    const body = await response.json();
+    return(body);
+}
 
 export default (req: NowRequest, res: NowResponse) => {
   const { id } = req.query;
-  res.status(200).json({ user: users[Number(id)] });
+  res.status(200).json(f1());
 };
