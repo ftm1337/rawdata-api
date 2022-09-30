2d1a49ca269c3e3fa2f22956bb03662cd5621dba
ftm1337 <84405345+ftm1337@users.noreply.github.com>
2022-09-30 08:07:04 -0400

    Create s2.js

---
+++ api/s2.js
@@ -0,0 +1,23 @@
+module.exports = (req, res) => {
+    res.json({
+      msg: 'hello there, how are you!!',
+      max: 1337,
+      total: 1336.9999,
+      circulating: 250,
+      burned: 17.8501337,
+      reserved: 671,
+      locked: 384,
+      outstanding: 666,
+      burns: {
+        burn: 0.00001337,
+        zero: 17.85,
+        dead: 0
+      },
+      locks: {
+        timelock: 384
+      },
+      treasury: 0,
+      team: 0,
+      timestamp : '2022-10-01'
+    });
+  };
